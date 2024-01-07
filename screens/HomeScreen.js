36176d6f1405c26afe564f58da4fc4fa303c2b73
Sharmaine Payong <sharmainerpayong@gmail.com>
2024-01-07 01:37:35 +0100

    Debug code

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -3,22 +3,21 @@ import React, { useLayoutEffect } from 'react';
 import {
   Image,
   SafeAreaView,
+  ScrollView,
   StyleSheet,
   Text,
   TextInput,
   View,
 } from 'react-native';
 import {
-  AdjustmentsIcon,
+  AdjustmentsHorizontalIcon,
   ChevronDownIcon,
-  SearchIcon,
+  MagnifyingGlassIcon,
   UserIcon,
 } from 'react-native-heroicons/outline';
+import Categories from '../components/categories';
 
 const styles = StyleSheet.create({
-  text: {
-    color: 'red',
-  },
   imageStyle: {
     width: 50, // Set the width and height for your image
     height: 50,
@@ -35,41 +34,46 @@ export default function HomeScreen() {
     });
   }, []);
 
-  // Reference the image using require from the assets directory
-  const localImage = require('../assets/photo1.png'); // Replace with the actual image path
+  const localImage = require('../assets/photo1.png');
 
   return (
-    <SafeAreaView style={{ backgroundColor: 'white', paddingTop: 5 }}>
-      <Text style={styles.text}>
-        <View className="flex-row pb-3 items-center mx-4 space-x-2">
-          <Image
-            source={localImage} // Use the imported local image as the source
-            style={styles.imageStyle} // Apply the styles using the style prop
-          />
-          <View className="flex-1">
-            <Text className="font-bold text-gray-400 text-xs">
-              Deliver Now!
-            </Text>
-            <Text className="font-bold text-xl">
-              Current Location
-              <ChevronDownIcon size={20} color="#00CCBB" />
-            </Text>
-          </View>
-          <UserIcon size={35} color="#00CCBB" />
+    <SafeAreaView className="bg-white pt-5">
+      <View className="flex-row pb-3 items-center mx-4 space-x-2">
+        <Image source={localImage} style={styles.imageStyle} />
+        <View className="flex-1">
+          <Text className="font-bold text-gray-400 text-xs">Deliver Now!</Text>
+          <Text className="font-bold text-xl">
+            Current Location
+            <ChevronDownIcon size={20} color="#00CCBB" />
+          </Text>
         </View>
+        <UserIcon size={35} color="#00CCBB" />
+      </View>
 
-        {/* Search */}
-        <View className="flex-row items-center space-x-2 pb-2 mx-4">
-          {/* <View className="flex-row flex-1 space-x-2 bg-gray-200 p-3">
-            <SearchIcon color="gray" size={20} />
-            <TextInput
-              placeholder="Restaurants and cuisines"
-              keyboardType="default"
-            />
-          </View> */}
-          {/* <AdjustmentsIcon style="#00CCBB" /> */}
+      {/* Search */}
+      <View className="flex-row items-center space-x-2 pb-2 mx-4 px-4">
+        <View className="flex-row flex-1 space-x-2 bg-gray-200 p-3">
+          <MagnifyingGlassIcon color="gray" size={20} />
+          <TextInput
+            placeholder="Restaurants and cuisines"
+            keyboardType="default"
+          />
         </View>
-      </Text>
+        <AdjustmentsHorizontalIcon color="#00CCBB" />
+      </View>
+
+      {/*/Content*/}
+      <ScrollView
+        className="bg-gray-100"
+        contentContainerStyle={{
+          paddingBottom: 100,
+        }}
+      >
+        {/*Categories */}
+        <Categories />
+
+        {/*Featured */}
+      </ScrollView>
     </SafeAreaView>
   );
 }
